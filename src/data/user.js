25dc9be42ab85acc044c6dae5814619fd4ddc605
Dimitar Petkov <mitfreex@gmail.com>
2023-09-25 00:11:13 +0300

    added session storage handling

--- src/data/user.js
+++ src/data/user.js
@@ -1,13 +1,31 @@
+import { setUserData } from '../util.js';
 import { post } from './api.js';
 
 export async function register(email, username, password) {
 
-    const result = await post('/users', { email, username, password });
+    const { sessionToken, objectId } = await post('/users', { email, username, password });
 
+    const userData = {
+        objectId,
+        email,
+        username,
+        sessionToken
+    }
+
+    setUserData(userData);
     console.log(result);
 }
 
 export async function login(email, password) {
-    const result = post('/login', { email, password });
+
+    const { email, username, objectId, sessionToken } = post('/login', { email, password });
+
+    const userData = {
+        objectId,
+        email,
+        username,
+        sessionToken
+    }
+
     console.log(result);
 }
