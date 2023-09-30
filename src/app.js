c94f84b24c8d1da67f2adc65933025a45e67f9f1
Dimitar Petkov <mitfreex@gmail.com>
2023-09-30 17:11:51 +0300

    preload middleware for room details added

--- src/app.js
+++ src/app.js
@@ -10,6 +10,8 @@ import { registerView } from "./views/register.js";
 import { navTemplate } from "./views/nav.js";
 import { homeView } from "./views/home.js";
 import { logoutAction } from "./views/logout.js";
+import { preload } from "./middlewares/preloader.js";
+
 
 
 
@@ -21,7 +23,7 @@ page(addUserNav(navTemplate))
 page('/index.html', '/')
 page('/', homeView);
 page('/rooms', catalogView);
-page('/rooms/:id', ({ params: { id } }) => console.log('details', id));
+page('/rooms/:id',preload('id','rooms'), ({ params: { id }, data }) => console.log('details', id, 'DATA', data)); //destructured ctx
 page('/host', createView);
 page('/login', loginView);
 page('/register', registerView);
